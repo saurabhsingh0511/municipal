665b4cf95ceb5e3d9360fef8952555da41dd5b3e
Saurabh Singh <saurabhsingh3198@gmail.com>
2024-09-11 13:33:39 +0530

    fix side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Sidebar from "./components/sidebar";
 import HomeSection from "./components/homesection";
 import { useState } from "react";
 
 function App() {
-  const counter = useSelector((state) => state.myReducer.myVariable);
-  const test = useSelector((state) => state.myReducer.test);
-  const dispatch = useDispatch();
   const [isClosed, setIsClosed] = useState(false);
 
   const toggleSidebar = () => {
